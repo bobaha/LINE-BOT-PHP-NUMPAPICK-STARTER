4bcfa99df10b2857d2847e0fc438500a5b997afb
bobaha <43331988+bobaha@users.noreply.github.com>
2021-10-03 10:19:42 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' =>  $userId,
+        'to' =>  'U4b13fe4c35abc1e83fa29b738341c906',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
