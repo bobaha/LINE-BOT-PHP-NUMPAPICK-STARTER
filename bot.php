5fce7045b7737121070806f84a39255eb0f5eb74
bobaha <43331988+bobaha@users.noreply.github.com>
2021-10-03 10:15:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,12 +8,12 @@ $content = file_get_contents('php://input');
 
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
+// if (!is_null($events['ESP'])) {
 	
-	send_LINE($events['ESP']);
+// 	send_LINE($events['ESP']);
 		
-	echo "OK";
-	}
+// 	echo "OK";
+// 	}
 if (!is_null($events['events'])) {
 	echo "line bot";
 	// Loop through each event
