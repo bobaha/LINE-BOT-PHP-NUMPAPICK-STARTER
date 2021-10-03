c54aabaf3655f9bf9e9506f1562e25dca2cd746c
bobaha <43331988+bobaha@users.noreply.github.com>
2021-10-03 10:40:44 +0700

    Update line.php

--- line.php
+++ line.php
@@ -30,14 +30,6 @@ function send_LINE($msg){
 //       curl_close($ch);
 
 //       echo $result . "\r\n"; 
- 
- $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('VA2W8ly0zQTI5AbsdXXaox1YSD7cKMbcI75vkHSDAV5LwBZ7PA3cRSBO/IwKMplEeGt+nyas/zZI8VPcwnAky1jICBp1+sM0F835CVtH2zvO6jGk/kUCgb7OBRb/hyx3E29VgVJW0q5sSLaL8A+D+gdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'a0045fd647592db211ed8af6b4013551']);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
-$response = $bot->replyMessage('U4b13fe4c35abc1e83fa29b738341c906', $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 }
 
 ?>
